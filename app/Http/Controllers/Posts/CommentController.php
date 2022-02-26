86ba59fc7baa266f053cd1c309d06118a55ed3cf
brito101 <rodrigobrito101@hotmail.com>
2022-02-25 22:15:02 -0300

    Settings notifications

--- app/Http/Controllers/Posts/CommentController.php
+++ app/Http/Controllers/Posts/CommentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Posts;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCommentFormRequest;
+use App\Models\Post;
+use App\Notifications\PostCommented;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -11,7 +13,8 @@ class CommentController extends Controller
     public function store(StoreCommentFormRequest $request)
     {
         $comment = $request->user()->comments()->create($request->all());
-
+        $author = (Post::where('id', $comment->post_id)->first())->user;
+        $author->notify(new PostCommented($comment));
         return redirect()->route('posts.show', $comment->post_id)
             ->with('status', 'Comentário realizado com sucesso!');
     }
